5a8e170a18e43f7eccb16642f0b742174c4a7f8a
Prince <p15301442@gmail.com>
2025-10-20 18:38:10 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'image.tmdb.org',
         port: '',
         pathname: '/**',
